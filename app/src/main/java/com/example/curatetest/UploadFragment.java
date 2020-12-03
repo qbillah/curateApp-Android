339f35072276f5e411a5dc3aade5805ee444f1db
Carolyn Gonzalez <carolyng.14.cr@gmail.com>
2020-12-02 20:24:28 -0500

    In fragment_upload.xml, I updated the blank fragment layout and added the image, close button, post button, and a description/caption of the image. In UploadFragment.java, I added the functionality to the close and post buttons as well as implemented a crop image activity. I Created 2 methods in UploadFragment.java, to upload a post: uploadImage() and onActivityResult().
    In AndroidManifest.xml and build.gradle, I added an activity/implementation to crop the image.

--- app/src/main/java/com/example/curatetest/UploadFragment.java
+++ app/src/main/java/com/example/curatetest/UploadFragment.java
@@ -1,12 +1,40 @@
 package com.example.curatetest;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +42,15 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class UploadFragment extends Fragment {
+
+    Uri imageUri;
+    String myURL;
+    StorageTask uploadTask;
+    StorageReference storageReference;
+
+    ImageView close, image_added;
+    TextView post;
+    EditText description;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,12 +90,140 @@ public class UploadFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_upload, container, false);
+
+        close = rootView.findViewById(R.id.close); //'x' button
+        image_added = rootView.findViewById(R.id.image_added);
+        post = rootView.findViewById(R.id.post); //'post' button
+        description = rootView.findViewById(R.id.description); //caption/description
+
+        storageReference = FirebaseStorage.getInstance().getReference("posts");
+
+        //if the user clicks on the 'x'
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UploadFragment.this.getActivity(), MainActivity.class));
+                getActivity().finish();
+
+            }
+        });
+
+        //if the user clicks on upload
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //takes them to upload image function
+                uploadImage();
+            }
+        });
+
+        //crop image activity
+        CropImage.activity()
+                .setAspectRatio(1,1)
+                .start(UploadFragment.this.getActivity()); //if this doesnt work change to .start(getContext(), this);
+
+        return rootView;
+    }
+
+    private String getFileExtension(Uri uri)
+    {
+        ContentResolver contentResolver = getActivity().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage()
+    {
+        //placeholder - replace this with a progress bar
+        final ProgressDialog progressDialog = new ProgressDialog(this.getActivity());
+        progressDialog.setMessage("Posting");
+        progressDialog.show();
+
+        if (imageUri != null)  //error is here?
+        {
+            final StorageReference filereference = storageReference.child(System.currentTimeMillis() + "."
+                    + getFileExtension(imageUri));
+
+            uploadTask = filereference.putFile(imageUri);
+
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception
+                {
+                    if (!task.isSuccessful())
+                    {
+                        throw task.getException();
+                    }
+
+                    return filereference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task)
+                {
+                    if (task.isSuccessful())
+                    {
+                        Uri downloadUri = task.getResult();
+                        myURL = downloadUri.toString();
+
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+                        String postid = reference.push().getKey();
+
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("postid", postid);
+                        hashMap.put("postimage", myURL);
+                        hashMap.put("description", description);
+                        hashMap.put("publisher", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                        reference.child(postid).setValue(hashMap);
+
+                        progressDialog.dismiss();
+
+                        startActivity(new Intent(UploadFragment.this.getActivity(), MainActivity.class));
+                        getActivity().finish();
+                    } else {
+                        Toast.makeText(UploadFragment.this.getActivity(), "Failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(UploadFragment.this.getActivity(), "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        } else{
+            Toast.makeText(UploadFragment.this.getActivity(), "No Image Selected!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //NOTE: Since RESULT_OK is constant of Activity class. In Activity class you can access directly
+        // but in other classes you need to write class name (Activity) also.
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK)
+        {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+
+            image_added.setImageURI(imageUri);
+        }
+        else {
+            Toast.makeText(this.getActivity(), "Something went wrong!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(UploadFragment.this.getActivity(), MainActivity.class));
+            getActivity().finish();
+
+        }
     }
 }
